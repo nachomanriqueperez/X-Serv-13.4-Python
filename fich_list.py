f40d5a6d74c332b033e40ece66e000a071f417b0
nachomanriqueperez <joseim@lambda38.aulas.gsyc.es>
2015-01-27 12:50:23 +0100

    Hecho ejercicio sin el diccionario

---
+++ fich_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+fich=open("/etc/passwd","r")
+num_usuarios=fich.readlines()
+print "El numero de usuarios de este ordenador es: " + str(len(num_usuarios))
+fich.close()
+
+# Primera parte hecha
+
+dicc = {}
+
+fich=open("/etc/passwd","r")
+
+for user in num_usuarios:
+    
+    conf = user.split(":");
+    username = conf[0]
+    shell = conf[-1]
+    print "El usuario: *" + username + "* utiliza la shell: " + shell
+
+dicc [username,shell]
+
+fich.close()
